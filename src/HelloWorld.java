fffba346203f34d4d8bc08d070c546bffe2e0bd9
sm <sm@LAPTOP-A2H5C581>
2023-05-24 18:40:01 +0900

    hi

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hi World");
-		System.out.println(123);
+		System.out.println("Hello World");
+		System.out.println(1234);
 	}
 }
